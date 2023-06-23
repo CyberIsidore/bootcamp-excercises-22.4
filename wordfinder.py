a4d0f0e7908f76b92d080182a23f545071f8368c
CyberIsidore <wizardisidore@gmail.com>
2023-06-23 12:44:45 -0400

    Complete!

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,21 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    def __init__(self, path):
+        dict_file = open(path)
+        self.words = self.parse(dict_file)
+        print(f"{len(self.words)} words read.")
+
+
+    def parse(self, dict_file):
+        return [w.strip() for w in dict_file
+                if w.strip() and not w.startswith('#')]
+
+
+    def random(self):
+        return random.choice(self.words)
+
+
+wf = WordFinder(r"//wsl.localhost/Ubuntu-22.04/home/izzy_online/pythonprojects/bc_oop_python/python-oo-practice/bootcamp-excercises-22.4/words.txt")
+print(f"Random word: {wf.random()}")
